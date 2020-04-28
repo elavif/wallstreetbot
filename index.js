a496a3540174259e48ef4bc9dd5da71d5df046b5
Eilon Lavi <eilonlavi@Eilons-MacBook-Pro.local>
2020-04-28 02:02:30 -0400

    debug logging

--- index.js
+++ index.js
@@ -113,6 +113,7 @@ function callSendAPI(sender_psid, message) {
       },
       "message": message
     }
+    console.log(request_body);
     // Send the HTTP request to the Messenger Platform
     request({
       "uri": "https://graph.facebook.com/v2.6/me/messages",
