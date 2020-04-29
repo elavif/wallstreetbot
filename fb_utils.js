7aa15f5b2e265602e33d141770c7159ba247b1ed
Eilon Lavi <eilonlavi@Eilons-MacBook-Pro.local>
2020-04-28 22:25:00 -0400

    bugfix

--- fb_utils.js
+++ fb_utils.js
@@ -1,27 +1,5 @@
 module.exports = {
 
-  // Handles messages events
-  handleMessage: function(sender_psid, received_message) {
-      
-      // for POC, send back the message, reversed.
-      if (received_message.text){
-        var reversed_text = received_message.text.split("").reverse().join("");
-        sendText(sender_psid, reversed_text);
-      }
-  },
-
-  // Handles messaging_postbacks events
-  handlePostback: function (sender_psid, received_postback) {
-
-  },
-
-
-  sendText: function (sender_psid, reply){
-    callSendAPI(sender_psid, {
-          "text": message
-        });
-  },
-
   // Sends response messages via the Send API
   callSendAPI: function callSendAPI(sender_psid, message) {
       // Construct the message body
@@ -45,5 +23,26 @@ module.exports = {
           console.error("Unable to send message:" + err);
         }
       }); 
+  },
+
+  sendText: function (sender_psid, reply){
+    callSendAPI(sender_psid, {
+          "text": message
+        });
+  },
+
+  // Handles messages events
+  handleMessage: function(sender_psid, received_message) {
+      
+      // for POC, send back the message, reversed.
+      if (received_message.text){
+        var reversed_text = received_message.text.split("").reverse().join("");
+        sendText(sender_psid, reversed_text);
+      }
+  },
+
+  // Handles messaging_postbacks events
+  handlePostback: function (sender_psid, received_postback) {
+
   },
 }
